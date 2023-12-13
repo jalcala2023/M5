fab3d008a126f2f1d13c7dfff47cc50621186a79
Jonay <jalcala2023@ilg.cat>
2023-12-13 12:30:42 +0100

    13/12/2023 12:30

--- Program.java
+++ Program.java
@@ -2,9 +2,9 @@
  * Program.java        20.10.2020
  *
  * Program to calculate the area and the perimeter
- * of a circle knowing the radius in meter.
+ * of a circleius in meter.
  *
- * Copyright 2020 Joan Sèculi <jseculi@escoladeltreball.org>
+ * Copy>
  *
  * This is free software, licensed under the GNU General Public License v3.
  * See http://www.gnu.org/licenses/gpl.html for more information.
