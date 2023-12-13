00e19bb581cfe08f560c29d6b8477ea63052aa78
Jonay <jalcala2023@ilg.cat>
2023-12-13 12:34:20 +0100

    Revert "13/12/2023 12:30"
    This reverts commit fab3d008a126f2f1d13c7dfff47cc50621186a79.

--- Program.java
+++ Program.java
@@ -2,9 +2,9 @@
  * Program.java        20.10.2020
  *
  * Program to calculate the area and the perimeter
- * of a circleius in meter.
+ * of a circle knowing the radius in meter.
  *
- * Copy>
+ * Copyright 2020 Joan Sèculi <jseculi@escoladeltreball.org>
  *
  * This is free software, licensed under the GNU General Public License v3.
  * See http://www.gnu.org/licenses/gpl.html for more information.
